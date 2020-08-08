b45136721085e2a063dffd1a5ff8820059117a98
wangshuai21 <1920357970@qq.com>
2020-08-08 17:21:59 +0800

    添加文件

---
+++ HelloGit/src/main/java/com/atsansan/git/HelloGit.java
@@ -0,0 +1,11 @@
+package com.atsansan.git;
+
+/**
+ * @author sansan
+ * @create 2020-08-08 16:37
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
